61a03d714b09cee68addbe405ba2371a41872de4
Tony Atkins <tony@raisingthefloor.org>
2016-02-02 17:04:12 +0100

    GPII-1241:  Pared down duplicate "sane response" check in favor of common function from {{gpii-express}}.

--- tests/lib/saneresponse.js
+++ tests/lib/saneresponse.js
@@ -1,18 +1,13 @@
 // A common function to confirm that the response sent by the server meets our standards.
 "use strict";
 var fluid  = require("infusion");
-var jqUnit = require("node-jqunit");
 var gpii   = fluid.registerNamespace("gpii");
 
 require("../lib/superset");
 
 fluid.registerNamespace("gpii.pouch.lucene.tests");
 gpii.pouch.lucene.tests.isSaneResponse = function (response, body, statusCode, expected) {
-    statusCode = statusCode ? statusCode : 200;
-
-    jqUnit.assertEquals("The response should have a reasonable status code", statusCode, response.statusCode);
-
-    jqUnit.assertValue("There should be a body.", body);
+    gpii.express.tests.helpers.isSaneResponse(response, body, statusCode);
 
     if (expected) {
         var jsonData = typeof body === "string" ? JSON.parse(body) : body;
