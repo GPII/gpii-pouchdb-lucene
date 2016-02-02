76430fbb2283e7a3a5cefa6faf8c6ebf87bd26d1
Tony Atkins <tony@raisingthefloor.org>
2016-02-02 17:05:11 +0100

    GPII-1241:  Cleaned up test wiring to take advantage of new {{sequenceEnd}} option in {{gpii-express}} case holder.  Removed outdated function.

--- tests/js/lucene-tests.js
+++ tests/js/lucene-tests.js
@@ -9,21 +9,24 @@ var gpii  = fluid.registerNamespace("gpii");
 require("./test-harness");
 require("../lib/saneresponse");
 
-var jqUnit = require("node-jqunit");
-
 var kettle = require("kettle");
 kettle.loadTestingSupport();
 
-fluid.registerNamespace("gpii.pouch.lucene.tests");
 require("gpii-express");
 gpii.express.loadTestingSupport();
 
-gpii.pouch.lucene.tests.expectNothing = function () {
-    jqUnit.expect(0);
-};
 
 fluid.defaults("gpii.pouch.lucene.tests.caseHolder", {
     gradeNames: ["gpii.express.tests.caseHolder"],
+    sequenceEnd: [
+        {
+            func: "{testEnvironment}.harness.lucene.events.onReadyForShutdown.fire"
+        },
+        {
+            listener: "fluid.identity",
+            event:    "{testEnvironment}.harness.lucene.events.onShutdownComplete"
+        }
+    ],
     expected: {
         // Because of limitations when comparing array data, we have to split each expected set of results into pieces,
         // one piece that can be compared with the overall results (overall), and one piece that can be compared to
@@ -53,13 +56,6 @@ fluid.defaults("gpii.pouch.lucene.tests.caseHolder", {
                             listener: "gpii.pouch.lucene.tests.isSaneResponse",
                             event:    "{basicRequest}.events.onComplete",
                             args:     ["{basicRequest}.nativeResponse", "{arguments}.0", 200, "{testCaseHolder}.options.expected.basic"]
-                        },
-                        {
-                            func: "{testEnvironment}.harness.lucene.events.onReadyForShutdown.fire"
-                        },
-                        {
-                            listener: "fluid.identity",
-                            event:    "{testEnvironment}.harness.lucene.events.onShutdownComplete"
                         }
                     ]
                 },
@@ -74,13 +70,6 @@ fluid.defaults("gpii.pouch.lucene.tests.caseHolder", {
                             listener: "gpii.pouch.lucene.tests.isSaneResponse",
                             event:    "{sortedRequest}.events.onComplete",
                             args:     ["{sortedRequest}.nativeResponse", "{arguments}.0", 200, "{testCaseHolder}.options.expected.sorted"]
-                        },
-                        {
-                            func: "{testEnvironment}.harness.lucene.events.onReadyForShutdown.fire"
-                        },
-                        {
-                            listener: "fluid.identity",
-                            event:    "{testEnvironment}.harness.lucene.events.onShutdownComplete"
                         }
                     ]
                 }
